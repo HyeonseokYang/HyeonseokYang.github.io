107a41d52c47bb6c7b08b5b40f69fb9bf14ea4ed
HyeonseokYang <57899117+HyeonseokYang@users.noreply.github.com>
2021-03-25 17:34:23 +0900

    Update postmessage.js

--- postmessage.js
+++ postmessage.js
@@ -1,7 +1,7 @@
  	window.addEventListener('message', function (e) {
 			if(e.data=="e1"){	
- 		   document.querySelector("#txtNexonID").value="gustjr5702";
- 		   document.querySelector("#txtPWD").value="rkskekfk1!";
+ 		   document.querySelector("#txtNexonID").value="abc";
+ 		   document.querySelector("#txtPWD").value="123";
  		   document.querySelector("#btnLogin").click();
 			}else if(e.data=="e2"){
 				GnxGameStartOnClick();
