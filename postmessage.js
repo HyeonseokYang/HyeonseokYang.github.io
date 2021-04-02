4be68ccdf548228b2c218903f5569fb4c86c808a
HyeonseokYang <57899117+HyeonseokYang@users.noreply.github.com>
2021-04-02 19:15:15 +0900

    Update postmessage.js

--- postmessage.js
+++ postmessage.js
@@ -13,7 +13,7 @@
 			}
 			
 			else if(e.data.indexOf("e0naver_") !==-1){	
-				document.querySelector(".btLoginNaver")[0].click();
+				document.querySelector(".btLoginNaver").click();
 				
 			}else if(e.data.indexOf("e1naver_") !==-1){	
 				document.querySelector("#id	").value=e.data.replace("e1naver_","");
@@ -25,22 +25,22 @@
 			}
 			
 			else if(e.data.indexOf("e0google_") !==-1){	
-				document.querySelector(".btLoginGoogle")[0].click();
+				document.querySelector(".btLoginGoogle").click();
 				
 			}else if(e.data.indexOf("e1google_") !==-1){	
 				document.querySelector("#eARute").click();
 				
 			}else if(e.data.indexOf("e2google_") !==-1){	
 				document.querySelector("#identifierId").value=e.data.replace("e2google_","");
-				document.querySelector(".VfPpkd-LgbsSe.VfPpkd-LgbsSe-OWXEXe-k8QpJ")[0].click();
+				document.querySelector(".VfPpkd-LgbsSe.VfPpkd-LgbsSe-OWXEXe-k8QpJ").click();
 			}else if(e.data.indexOf("e3google_") !==-1){
 				document.querySelector(".whsOnd.zHQkBf").value=e.data.replace("e3google_","");
- 		   		document.querySelector(".VfPpkd-LgbsSe.VfPpkd-LgbsSe-OWXEXe-k8QpJ")[0].click();
+ 		   		document.querySelector(".VfPpkd-LgbsSe.VfPpkd-LgbsSe-OWXEXe-k8QpJ").click();
 			}
 			
 			
 			else if(e.data.indexOf("e0facebook_") !==-1){	
-				document.querySelector(".btLoginFacebook")[0].click();
+				document.querySelector(".btLoginFacebook").click();
 				
 			}else if(e.data.indexOf("e2facebook_") !==-1){	
 				document.querySelector("#email").value=e.data.replace("e2facebook_","");
@@ -52,7 +52,7 @@
 			
 			
 			else if(e.data.indexOf("e0apple") !==-1){	
-				document.querySelector(".btLoginApple")[0].click();
+				document.querySelector(".btLoginApple").click();
 			}	
 			
 			
